aa35c6855b6f2ddecb16f7b1ade596cdd7de8e37
Admin <mramazanov@outlook.com>
2025-04-02 10:19:54 +0300

    init

---
+++ src/main/java/ru/javajabka/taskservice/TaskserviceApplication.java
@@ -0,0 +1,11 @@
+package ru.javajabka.taskservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskserviceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TaskserviceApplication.class, args);
+	}
+}
